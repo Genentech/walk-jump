34f91a963c35615cb4ae3b530080cd91fe7040cb
djberenberg <berenbed@gene.com>
2023-05-31 12:43:08 -0400

    dataset

--- src/walkjump/data/_batch.py
+++ src/walkjump/data/_batch.py
@@ -15,7 +15,7 @@ class Batch:
 
     def __post_init__(self, token_list: list[str]):
         self.alphabet = LabelEncoder().fit(token_list)
-    
+
     @property
     def vocab_size(self) -> int:
         return len(self.alphabet.classes_)
@@ -27,7 +27,7 @@ class Batch:
 
         packed_batch = torch.stack(inputs, dim=0)
         return cls(packed_batch, token_list)
-    
+
     @cached_property
     def x(self) -> torch.Tensor:
         return torch.nn.functional.one_hot(self.batch_tensor, num_classes=self.vocab_size).float()
